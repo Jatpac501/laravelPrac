8c8597a8240a1aad18b7da7ba430a71406eb24ba
h4rdfox <jatpac501@gmail.com>
2023-10-18 01:22:33 +0400

    add migrations and models

---
+++ app/Models/Schedule.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    protected $table = 'schedule';
+    protected $fillable = ['Week', 'dayWeek', 'Сouple', 'group_id', 'teacher_id', 'subject_id', 'room_id' ];
+    public function group()
+    {
+        return $this->belongsTo(Group::class, 'group_id');
+    }
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class, 'teacher_id');
+    }
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class, 'subject_id');
+    }
+    public function room()
+    {
+        return $this->belongsTo(Room::class, 'room_id');
+    }
+}
+
